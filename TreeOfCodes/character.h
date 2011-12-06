a68693274da32a1d63c946e7691043e2408bbf69
agardne2 <agardne2@mail.umw.edu>
2011-12-06 13:34:24 -0500

    Update TreeOfCodes/character.h

--- TreeOfCodes/character.h
+++ TreeOfCodes/character.h
@@ -16,11 +16,18 @@ Public{
    //returns true when the character is feeling free, and false when they're not
   bool isOutOfHouse() const;
   //used for when the character is out of the house
-  //returns true when the character is out of the house, and false when they're not
+  //returns true when the character is out of the house, and false when 
   bool isMissingMother() const;
   //used for when the character is missing Mother
         //returns true when the character is missing Mother and false when they're not
   bool isInCorner() const;
   //used for when the character is in the corner
   //returns true when the character is in the corner, and false when they're not
+  bool isAngry() const;
+  //used for when character is begging God at night
+  //used for when character is pleading to recocile with Mother
+  bool isWorried() const;
+  //used when character has stopped going out
+  //used when character is feeling absentminded
+  
 }
