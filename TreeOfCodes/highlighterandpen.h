c83f48d9838fcc4c1b5d0167db65be9d6ab10b76
agardne2 <agardne2@mail.umw.edu>
2011-12-07 18:59:27 -0500

    Update TreeOfCodes/highlighterandpen.h

--- TreeOfCodes/highlighterandpen.h
+++ TreeOfCodes/highlighterandpen.h
@@ -4,3 +4,5 @@
 * Keep a variable called poetic or flow or something that keeps track */
 * eyes/half (closed,naked,animal,shameless)/silence
 * shadows/anger
+*sense of human feeling (shown throughout characters of mother/father/narr)
+
