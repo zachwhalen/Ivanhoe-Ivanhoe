c74a9b014374e29c515a076c754c7b809bbb4c9c
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2011-12-08 12:45:18 -0500

    fail'd at making encoder work

--- RawSharkTexts/encoder.rb
+++ RawSharkTexts/encoder.rb
@@ -1,24 +1,36 @@
 class Encoder
 
-  attr_accessor :text, :code
-  @code = [[s, [w, q, a, z, x, c, d, e]],
-          [d, [e, w, s, x, c, v, f, r]],
-          [f, [r, e, d, c, v, b, g, t]],
-          [g, [t, r, f, v, b, n, h, y]],
-          [h, [y, t, g, b, n, m, j, u]],
-          [j, [u, y, h, n, m, z, k, i]],
-          [k, [i, u, j, m, z, l, o, 
+  attr_accessor :text, :code, :encoded
+  @code = Hash.new([' ', [' '],
+          ['s', ['w', 'q', 'a', 'z', 'x', 'c', 'd', 'e']],
+          ['d', ['e', 'w', 's', 'x', 'c', 'v', 'f', 'r']],
+          ['f', ['r', 'e', 'd', 'c', 'v', 'b', 'g', 't']],
+          ['g', ['t', 'r', 'f', 'v', 'b', 'n', 'h', 'y']],
+          ['h', ['y', 't', 'g', 'b', 'n', 'm', 'j', 'u']],
+          ['j', ['u', 'y', 'h', 'n', 'm', 'z', 'k', 'i']],
+          ['k', ['i', 'u', 'j', 'm', 'z', 'a', 'l', 'o']],
+          ['l', ['o', 'i', 'k', 'z', 'x', 'a', 'q', 'p']],
+          ['x', ['s', 'a', 'z', 'q', 'w', 'e', 'c', 'd']],
+          ['c', ['d', 's', 'x', 'w', 'e', 'r', 'v', 'f']],
+          ['v', ['f', 'd', 'c', 'e', 'r', 't', 'b', 'g']],
+          ['b', ['g', 'f', 'v', 'r', 't', 'y', 'n', 'h']]])
 
+  def new(text)
+    @text=text
+  end
 
-  def read(text)
-    File.foreach(text) do |s|
-    @text << parse(s)
+  def read
+    File.foreach(@text) do |s|
+      @text << parse(s)
+    end
   end
 
   def parse(string)
+    randNum = rand(8)
 
+    if (@code.key?(string))
+      @final += @code.to_a[randNum]  
+    end
   end
 
 end
-
-puts "Please enter the filename of a textfile to encode"
