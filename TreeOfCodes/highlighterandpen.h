65605e89986132fd08fd2c60e22cba5523072cdb
agardne2 <agardne2@mail.umw.edu>
2011-12-07 19:59:08 -0500

    Update TreeOfCodes/highlighterandpen.h

--- TreeOfCodes/highlighterandpen.h
+++ TreeOfCodes/highlighterandpen.h
@@ -2,7 +2,9 @@
 * Keep all mother/fathers/children
 * Alone/darkness/empty
 * Keep a variable called poetic or flow or something that keeps track */
+*use of allegories (weeks pass like boats...)
 * eyes/half (closed,naked,animal,shameless)/silence
 * shadows/anger
 *sense of human feeling (shown throughout characters of mother/father/narr)
+
 
