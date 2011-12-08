1b1a8113f7524d86495031897837e68746f66fe2
Shannon <sswanson@mail.umw.edu>
2011-12-08 10:33:41 -0500

    Update TreeOfCodes/highlighterandpen.h

--- TreeOfCodes/highlighterandpen.h
+++ TreeOfCodes/highlighterandpen.h
@@ -7,8 +7,31 @@
 * shadows/anger
 *sense of human feeling (shown throughout characters of mother/father/narr)
 
+#import "treeofcodes.h"
+#import "StreetOfCrocodiles.h"
+#import <ifstream>
 
 Public{
-    poeticWords[1000] = new array[1000]; //this creates a corpus of applicable words and stores them in a list
-    
+    string poeticWords[1000] = new array[1000]; //this creates a corpus of applicable words and stores them in a list
+    ifstream in("StreetofCrocodiles.h"); //Reads in the text file
+    While (inFile){
+    String line=br.readLine();
+    string streetOfCrocodilesLines[1000000] = new array[100000];//stores the whole street of crocodiles text line by line
+    if (line=="street of crocodiles" or line=="Street of Crocodiles"){
+      line="tree of codes"; //changes "street of crocodiles" to "tree of codes whenever it finds it"
+    }
+    if (line=="Bruno Schulz"){
+      line="Jonathan Safran Foer"
+          }//changes the author's name whenever it comes across it
+  }
+  
+  for (int i=0; i<streetOfCrocodilesLines.length; i++){//goes through entire story
+      for (int x=0; x<poeticWords.length; x++){//goes through the list of poetic words 
+      if (streetOfCrocodilesLines[i].contains(poeticWords[x])){//if the line contains the poeticword
+          write poeticWords[x] to treeofcodes.h; //how do I say this???
+      }
+      
+      }
+  }
+      
 }
