f3eb22832b6e82f70d288e4e41416e8e4b77da0e
Shannon <sswanson@mail.umw.edu>
2011-12-08 13:38:42 -0500

    Update TreeOfCodes/character.h

--- TreeOfCodes/character.h
+++ TreeOfCodes/character.h
@@ -1,57 +1,58 @@
 Public{
-  bool isHappy() const;
+  int isHappy() const;
   //used for when the character is feeling happy
   //returns true when the character is happy, and false when they're not
-  bool isSad() const;
+  int isSad() const;
   //used for when the character is feeling sad
   //returns true when the character is sad, and false when they're not
-  bool isLonely() const;
+  int isLonely() const;
   //used for when the character is feeling lonely
   //returns true when the character is lonely, and false when they're not
-  bool isGuilty() const;
+  int isGuilty() const;
   //used for when the character is feeling guilty
   //returns true when the character is guilty, and false when they're not
-  bool isFeelingFree() const;
+  int isFeelingFree() const;
    //used for when the character is feeling free of responsiblities
    //returns true when the character is feeling free, and false when they're not
-  bool isOutOfHouse() const;
+  int isOutOfHouse() const;
   //used for when the character is out of the house
   //returns true when the character is out of the house, and false when 
-  bool isMissingMother() const;
+  int isMissingMother() const;
   //used for when the character is missing Mother
         //returns true when the character is missing Mother and false when they're not
-  bool isInCorner() const;
+  int isInCorner() const;
   //used for when the character is in the corner
   //returns true when the character is in the corner, and false when they're not
-  bool isAngry() const;
+  int isAngry() const;
   //used for when character is angry
   //returns when character is angry and false when they're not
-  bool isWorried() const;
+  int isWorried() const;
   //used when character is worried
   //used when character is worried and false when they're not
-  bool isResentful() const;
+  int isResentful() const;
   //used when character is resentful
   // used when character is resenful and false when they're not
-  bool isSobbing() const;
+  int isSobbing() const;
   //used when character is sobbing
   //used when character is sobbing and false when they're not
-  bool isDoubtful() const;
+  int isDoubtful() const;
   //used when character is doubtful 
   //used when character is doubtful and false when they're not
-  bool isLight() const;
+  int isLight() const;
   //used when character is light
   //used when character is light and false when they're not
-  bool isAwake() const;
+  int isAwake() const;
   //used when character is awake
   //used when character is awake and false when they're not
-  bool is CallingForHelp() const;
+  int is CallingForHelp() const;
   //used when character is calling for help
   //used when character is calling for help and false when they're not
-  bool is AbsentMinded() const;
+  int is AbsentMinded() const;
   //used when character is absentminded
   //used when character is absentminded and false when they're not
-  bool is Motionless() const;
+  int is Motionless() const;
   //used when character is motionless
   //used when character is motionless and false when they're not
   
 }
+
